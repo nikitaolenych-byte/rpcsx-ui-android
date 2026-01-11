cb2cb41562f1ce8646751473013e6721f7c2aac0
nikitaolenych-byte <nikitaolenych@gmail.com>
2026-01-11 16:03:42 +0000

    fix(ci): remove Kotlin DSL CMake args that break CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,33 +23,6 @@ android {
             abiFilters += listOf("arm64-v8a")  // Оптимізовано тільки для ARMv9
         }
 
-        externalNativeBuild {
-            cmake {
-                // ARMv9 + SVE2 оптимізації для Snapdragon 8s Gen 3
-                arguments.addAll(
-                    listOf(
-                        "-DCMAKE_BUILD_TYPE=Release",
-                        "-DANDROID_ARM_NEON=ON",
-                        "-DANDROID_STL=c++_shared",
-                    )
-                )
-
-                // Максимальні оптимізації компілятора
-                cppFlags.addAll(
-                    listOf(
-                        "-march=armv9-a+sve2",
-                        "-O3",
-                        "-ffast-math",
-                        "-ftree-vectorize",
-                        "-funroll-loops",
-                        "-flto=thin",
-                        "-fvisibility=hidden",
-                        "-fvisibility-inlines-hidden",
-                    )
-                )
-            }
-        }
-
         buildConfigField("String", "Version", "\"v${versionName}\"")
         buildConfigField("String", "OptimizationTarget", "\"Snapdragon 8s Gen 3 (ARMv9+SVE2)\"")
         buildConfigField("boolean", "EnableARMv9Optimizations", "true")
